ba671725f9bd84f1240d07c493db8a17e22195a7
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-09-14 08:44:34 +0200

    Deelbaar door vijf is niet deelbaar door 11

--- src/nl/novi/opdrachten/methodes/DeelbaarDoorVijf.java
+++ src/nl/novi/opdrachten/methodes/DeelbaarDoorVijf.java
@@ -2,7 +2,7 @@ package nl.novi.opdrachten.methodes;
 
 /**
  * Schrijf een methode die een Integer ontvangt.
- * Wanner het getal deelbaar is door 11, laat hem de volgende tekst returnen:
+ * Wanneer het getal deelbaar is door 5, laat hem de volgende tekst returnen:
  * 'deelbaar door vijf'
  * Wanneer het getal niet deelbaar is door vijf:
  * 'ondeelbaar door vijf'
