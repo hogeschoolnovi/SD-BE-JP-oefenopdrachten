20fdf02a5dc2e7d9c9d05acf888f9e7e2e35574e
MRensen <markrensen@hotmail.com>
2023-05-30 16:15:00 +0200

    uitdagend

--- src/nl/novi/opdrachten/lijsten/VersleutelNamenLijst.java
+++ src/nl/novi/opdrachten/lijsten/VersleutelNamenLijst.java
@@ -3,11 +3,15 @@ package nl.novi.opdrachten.lijsten;
 public class VersleutelNamenLijst {
 
     /**
+     * Uitdagend!!!
+     *
      * Bijgevoegd is verzetsleden.txt
      * Maak een programma dat verzetsleden.txt uitleest.
      * Versleutel de namen op dezelfde manier als in GeheimeCode.java
      * En sla de versleutelde namen op in secret.txt
      *
+     * Kijk in de uitleg.week2.io package naar een voorbeeld hoe je een tekstbestand kunt uitlezen.
+     * Tip: Je kunt de methodes in de GeheimCode klasse hier hergebruiken om de namen uit secret.txt te versleutelen.
      */
     public static void main(String[] args) {
 
